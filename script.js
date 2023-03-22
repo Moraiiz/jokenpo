f108a7e674f22963abcb94b0e79939010a207617
Kelvin Morais <58197094+Moraiiz@users.noreply.github.com>
2023-03-21 22:50:22 -0300

    Ajustando análise do resultado

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const winner = document.getElementById('winner')
 const loser = document.getElementById9('loser')
 const game = document.getElementById('gameplay')
 
+var you = ''
+var cpu = ''
+
 function rule() {
     menu.classList.add('none')
     rules.classList.remove('none')
@@ -20,7 +23,45 @@ function play(){
 }
 
 function image() {
-    var you = document.querySelector('input[name="choices"]:checked').value        
+    you = document.querySelector('input[name="choices"]:checked').value        
     playermove.innerHTML = "<img src='images/" + you + ".png'>"
-    cpumove.innerHTML = ''
+}
+
+function jokenpo(){
+    let option = ['rock', 'paper', 'scissors']
+    let num = Math.floor(Math.random() * ( 2 - 0 + 1)) + 0
+    cpu = option[num]
+    cpumove.innerHTML = "<img src='images/" + cpu + ".png'>"
+    analysis()
+}
+
+function analysis() {
+    
+    var playerscore = document.getElementById('pscore')
+    var cpuscore = document.getElementById('cscore')
+    playerscore = '0'
+    cpuscore = '0'
+    let win = '0'
+    
+    if(you == cpu) {
+
+    } else if(you == 'rock') {
+        win = cpu == 'scissors' ? 2 : 1
+    } else if(you == 'paper') {
+        win = cpu == 'rock' ? 2 : 1
+    } else if(you == 'scissors') {
+        win = cpu == 'paper' ? 2 : 1
+    }
+
+    if(win == 0) {
+
+    } else if(win == 2) {
+        playerscore = ++1
+    } else {
+        cpuscore = ++1 
+    }
+
+    pscore.innerHTML =  playerscore
+    cscore.innerHTML = cpuscore
+    
 }
